cc9125bf2871186e94ae9cf3870af0cdf7e92e05
Your Name <david@segura.org>
2026-08-12 08:17:34 -0500

    refactor(workflow): use core device disk check
    Validate target disk availability from OSDCoreDevice.LocalDisk instead of querying and storing fixed disk state in the workflow invocation object.

--- OSDCloud/private/steps/2-test/step-test-targetdisk.ps1
+++ OSDCloud/private/steps/2-test/step-test-targetdisk.ps1
@@ -5,16 +5,7 @@ function step-test-targetdisk {
     Write-Verbose -Message "[$(Get-Date -format s)] [$($MyInvocation.MyCommand.Name)] Start"
     $Step = $global:OSDCloudCurrentStep
     #=================================================
-    #region Main
-    if ($env:SystemDrive -eq 'X:') {
-        $global:OSDCloudWorkflowInvoke.GetDiskFixed = Get-DeviceLocalDisk | Where-Object { $_.IsBoot -eq $false } | Sort-Object Number
-    }
-    else {
-        $global:OSDCloudWorkflowInvoke.GetDiskFixed = Get-DeviceLocalDisk | Sort-Object Number
-    }
-
-
-    if ($global:OSDCloudWorkflowInvoke.GetDiskFixed) {
+    if ($global:OSDCoreDevice.LocalDisk) {
         Write-Host -ForegroundColor DarkGray "[$(Get-Date -format s)] [INFO] Fixed Disk is valid. OK."
     }
     else {
@@ -24,7 +15,6 @@ function step-test-targetdisk {
         Start-Sleep -Seconds 86400
         exit
     }
-    #endregion
     #=================================================
     Write-Verbose -Message "[$(Get-Date -format s)] [$($MyInvocation.MyCommand.Name)] End"
     #=================================================
